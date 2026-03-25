db6d3e2d538feba65a905ae306abbc88e1e0dc98
hager-ali225 <s-hager.abdelaziz@zewailcity.edu.eg>
2026-03-26 00:52:14 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,12 +1,21 @@
 import random
+import mlflow
+
+mlflow.start_run()
 
 accuracy = random.uniform(0.7, 0.95)
+
+mlflow.log_metric("accuracy", accuracy)
+
+run_id = mlflow.active_run().info.run_id
 
 with open("accuracy.txt", "w") as f:
     f.write(str(accuracy))
 
 with open("model_info.txt", "w") as f:
-    f.write("run_123")
+    f.write(run_id)
 
 print("Accuracy:", accuracy)
-print("Run ID: run_123")
+print("Run ID:", run_id)
+
+mlflow.end_run()
